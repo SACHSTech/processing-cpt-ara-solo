9086bc618017cdef10640c362093a4a624cf8732
lilnuggetcool <Ara.Torossian25@ycdsbk12.ca>
2024-06-06 11:31:55 -0400

    changed code now i only have 1 error

---
+++ Money.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+public class Money extends PApplet {
+  
+  public void settings() {
+    size(400, 400);
+  }
+
+  public void setup() {
+    background(255);
+  }
+
+  public void draw() {
+    // Your drawing code here
+  }
+  
+}
